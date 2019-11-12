075673f8abd3bbf5fd10f2723e79d8c268f0889d
Cristian Juarez <56379681+manases-juarez@users.noreply.github.com>
2019-11-11 20:43:04 -0600

    Add files via upload

--- VivoritaSerial/src/GUI/Login.java
+++ VivoritaSerial/src/GUI/Login.java
@@ -5,19 +5,30 @@
  */
 package GUI;
 
+import vivoritaserial.VivoritaSerial;
 import javax.swing.JOptionPane;
+import vivoritaserial.Run_app;
 
 /**
  *
  * @author PICHU
  */
 public class Login extends javax.swing.JFrame {
-
+    /*
+    Variables que se usan en toda la aplicación para llevar el control
+    del usuario y los usuarios
+    */
+    public static Run_app inicio = new Run_app();
+    public static String app_usuario = "";
+    public static String app_contraseña = "";
+    
     /**
      * Creates new form Login
      */
     public Login() {
         initComponents();
+        this.jPasswordField1.setText("12345");
+        this.jTextField1.setText("Admin_ORGA");
     }
 
     /**
@@ -31,34 +42,37 @@ public class Login extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
-        jButton6 = new javax.swing.JButton();
+        jPasswordField1 = new javax.swing.JPasswordField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Dialog", 0, 36)); // NOI18N
-        jLabel1.setText("Administrador");
+        jLabel1.setText("SNAKE");
 
-        jButton1.setText("Top 10 Tiempos");
+        jLabel2.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
+        jLabel2.setText("Usuario");
+
+        jLabel3.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
+        jLabel3.setText("Contraseña");
+
+        jButton1.setText("Iniciar sesión");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
 
-        jButton2.setText("Top 10 puntos");
-
-        jButton3.setText("Ver usuarios");
-
-        jButton4.setText("Eliminar usuarios");
-
-        jButton5.setText("jButton5");
-
-        jButton6.setText("jButton6");
+        jButton2.setText("Crear usuario");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -67,38 +81,42 @@ public class Login extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(77, 77, 77)
-                        .addComponent(jLabel1))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3))
+                        .addGap(37, 37, 37)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel1)
+                            .addComponent(jTextField1)
+                            .addComponent(jPasswordField1, javax.swing.GroupLayout.DEFAULT_SIZE, 175, Short.MAX_VALUE)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(141, 141, 141)
+                        .addGap(155, 155, 155)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jButton2)
-                            .addComponent(jButton1)
-                            .addComponent(jButton3)
-                            .addComponent(jButton4)
-                            .addComponent(jButton5)
-                            .addComponent(jButton6))))
-                .addContainerGap(90, Short.MAX_VALUE))
+                            .addComponent(jButton1))))
+                .addContainerGap(81, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(42, 42, 42)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(44, 44, 44)
                 .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(18, 18, 18)
                 .addComponent(jButton2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton5)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton6)
-                .addContainerGap(38, Short.MAX_VALUE))
+                .addContainerGap(35, Short.MAX_VALUE))
         );
+
+        jLabel1.getAccessibleContext().setAccessibleName("SNAKE");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -120,10 +138,47 @@ public class Login extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    //Iniciar sesión
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        JOptionPane.showMessageDialog(null, "Muy pronto El top 10");
+        String usuario = this.jTextField1.getText();
+        String contraseña = this.jPasswordField1.getText();
+        if(usuario.equals("Admin_ORGA")){
+            if(contraseña.equals("12345")){
+                Administrador admin = new Administrador();
+                admin.setVisible(true);
+                this.setVisible(false);
+                //JOptionPane.showMessageDialog(null, "Eres el admin");
+            }else{
+                JOptionPane.showMessageDialog(null, "La contraseña no coincide con este usuario");
+            }
+        }else{
+            if(Login.inicio.existe_usuario(usuario)){
+                if(Login.inicio.iniciarSesion(usuario, contraseña) != null){
+                    Login.app_contraseña = contraseña;
+                    Login.app_usuario = usuario;
+                    GUI.Usuario a = new Usuario();
+                    a.setVisible(true);
+                    this.setVisible(false);
+                    //JOptionPane.showMessageDialog(null, "Usted se a autenticado");
+                    return;
+                }
+                JOptionPane.showMessageDialog(null, "La contraseña no coincide con el usuario" + usuario);
+            }else{
+                JOptionPane.showMessageDialog(null, "El usuario " + usuario + " no se ha registrado");
+            }
+        }
+        
+        
     }//GEN-LAST:event_jButton1ActionPerformed
+    
+    //Crear usuarios
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        Registrarse registro = new Registrarse();
+        registro.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -163,11 +218,11 @@ public class Login extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JButton jButton6;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
--- VivoritaSerial/src/vivoritaserial/VivoritaSerial.java
+++ VivoritaSerial/src/vivoritaserial/VivoritaSerial.java
@@ -6,6 +6,9 @@
 package vivoritaserial;
 
 import GUI.Login;
+import Usuarios.Usuario;
+import java.util.LinkedList;
+import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import org.pushingpixels.substance.api.SubstanceLookAndFeel;
@@ -20,13 +23,31 @@ public class VivoritaSerial {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        /*La aplicacion inica con el login, las estructuras de almacenamiento estan en Run_app
+            En el login se colocan las variables estaticas y dinamicas
+        */
+        Run_app inicio = new Run_app();
+        
+        /*
+        Usuarios de prueba
+        */
+        for(int i=0; i<25; i++){
+            Login.inicio.usuarios.add(new Usuario("a"+i, "123"));
+        }
+        Random r = new Random();
+        int var = 0;
+        for(Usuario i:Login.inicio.usuarios){
+            var = r.nextInt(100)+1;
+            i.setPuntos(var);
+            var = r.nextInt(100)+1;
+            i.setTiempo(var);
+        }
         // TODO code application logic here
         //JFrame.setDefaultLookAndFeelDecorated(true);
         //SubstanceLookAndFeel.setSkin("org.pushingpixels.substance.api.SubstanceSkin.BusinessSkin");
         //SubstanceLookAndFeel.se
         //UIManager.setLookAndFeel("");
-        Login login = new Login();
-        
+        Login login = new Login();        
         login.show();
     }
     
